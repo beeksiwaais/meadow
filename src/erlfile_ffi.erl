851ab0ec4a17b4cbfdfb89d1301ba077e18eafae
Beeksi Waais <181389+beeksiwaais@users.noreply.github.com>
2024-04-28 21:05:28 +0200

    add erlang to check for old file format -> mac binhex and dos

--- src/erlfile_ffi.erl
+++ src/erlfile_ffi.erl
@@ -45,6 +45,47 @@ contains_non_printable(Chunk) ->
         nomatch -> false
     end.
 
+% Check if the file is in Macintosh BinHex encoding format
+is_mac_binhex(FileName) ->
+    {ok, Binary} = file:read_file(FileName),
+    case is_mac_binhex_binary(Binary) of
+        true -> io:format("~s is in Macintosh BinHex encoding format.~n", [FileName]);
+        false -> io:format("~s is not in Macintosh BinHex encoding format.~n", [FileName])
+    end.
+
+is_mac_binhex_binary(Binary) ->
+    % BinHex files start with the ASCII characters ": (Colon) H (Capital H)"
+    <<":H", _/binary>> = Binary,
+    % Check if the file has a valid BinHex header
+    case is_binhex_header(Binary) of
+        true -> true;
+        false -> false
+    end.
+
+is_binhex_header(Binary) ->
+    % BinHex header contains information like file type, creator application, etc.
+    % We can check for certain patterns to determine if it's a valid header
+    <<_:4/binary, Version:4/binary, _:2/binary, Len:4/binary, _:2/binary, _/binary>> = Binary,
+    % Check if the header version is valid (typically it's "1")
+    case Version of
+        <<1:4>> -> true;
+        _ -> false
+    end.
+
+% Check if the file is in DOS binary format
+is_dos_binary(FileName) ->
+    {ok, Binary} = file:read_file(FileName),
+    case is_dos_binary_binary(Binary) of
+        true -> io:format("~s is in DOS binary format.~n", [FileName]);
+        false -> io:format("~s is not in DOS binary format.~n", [FileName])
+    end.
+
+is_dos_binary_binary(Binary) ->
+    % DOS binary files typically start with certain patterns
+    % For simplicity, we'll check if the first two bytes are "MZ" (an identifier for DOS executable files)
+    <<77, 90, _/binary>> = Binary, % MZ in ASCII
+    true.
+
 file_open(Path) ->
   case file:open(Path, [raw]) of
     {ok, Fd} ->
